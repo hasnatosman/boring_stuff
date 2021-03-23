cf0d9a3cdf9c966348aa9e55412be727970c9399
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-03-23 15:01:08 +0600

    Add files via upload

---
+++ alice.py
@@ -0,0 +1,10 @@
+name = input('Enter a name : ')
+if name == 'Alice':
+    age = input('How old are you ? ')
+    if age >= '18':
+        print('Hi Alice !')
+        print('How grown up.!')
+    else:
+        print('You look so young.')
+else:
+    print('He is a stranger.')
