2cd10f00c32acdbca8ef10786edb460150383ad5
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-03-23 17:40:26 +0600

    Add files via upload

---
+++ kiddo.py
@@ -0,0 +1,12 @@
+name = input('Enter a name: ')
+
+if name == 'Alice':
+    print('Hi ! Alice .')
+else:
+    age = input('Input an age: ')
+    if age >= '18':
+        print('You are not alice, Who are you?')
+    else:
+        print('You must be Alice')
+
+
