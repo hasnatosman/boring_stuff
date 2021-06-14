7f48e22415adc7d91b34155bf6f5002d4371f7c3
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-06-14 09:12:27 +0600

    Add files via upload

---
+++ what.py
@@ -0,0 +1,13 @@
+print('Hello ')
+print('Hello ! How are you?')
+
+name = input('What is your name ? ')
+
+print('Oh', name, 'You looks so friendly .!')
+
+print('Thanks')
+
+
+age = int(input('How older are you? '))
+
+print('Next year you will be ' + str(int(age) + 1)+ ' years old' )
